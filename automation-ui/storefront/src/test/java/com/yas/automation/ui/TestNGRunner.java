c79c00eacacf32f5976337ee2667efeeddc68f26
Phuoc Nguyen <phuoc.nguyenba@nashtechglobal.com>
2024-09-13 10:35:28 +0700

    #1022 - Apply Automation test for Yas project
    - create base project and write example login test case using cucumber and selenium

---
+++ automation-ui/storefront/src/test/java/com/yas/automation/ui/TestNGRunner.java
@@ -0,0 +1,12 @@
+package com.yas.automation.ui;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/resources/storefront/features",
+        glue = "com.yas.automation.ui",
+        plugin = {"pretty", "html:target/cucumber-reports"}
+)
+public class TestNGRunner extends AbstractTestNGCucumberTests {
+}
